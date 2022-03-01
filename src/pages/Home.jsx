0fda8a20aae2ac970412ed9d17ff80ed0b420f6c
Carol_Inglethe <carolinainglethe@gmail.com>
2022-03-01 16:43:31 -0300

    Pagina  home e releases - css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ function Home() {
 
   return (
     <div className="home-container">
+      
      <section className="home-section">
         <h1>Seja Bem vindo(a) ao Space-X</h1>
         <p>Veja os lançamentos e suas informações</p>
