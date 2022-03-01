a5cd3f5a5a452986d1cc29ab436c46fc8587429b
Carol_Inglethe <carolinainglethe@gmail.com>
2022-03-01 11:21:21 -0300

    CSS NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logoRocket from '../images/foguete-cinza.png'
 
 
 function NavBar() {
 
   return (
     <div className="navbar-container">
-         NavBar
+        <span>
+         <img src={logoRocket} alt="logo de foguete" width="70px"/>
+        </span>
          <Link to="/"> Inicio </Link>
          <Link to="/releases"> Lançamentos </Link>
     </div>
