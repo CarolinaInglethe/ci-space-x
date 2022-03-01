bdd570f3d3b3d03c68bf335daa8440c2dec813fa
Carol_Inglethe <carolinainglethe@gmail.com>
2022-03-01 17:00:28 -0300

    Releases - componente NavBarReleases - CSS

--- src/components/NavBarReleases.jsx
+++ src/components/NavBarReleases.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NavBarReleases = () => {
 
   return (
     <div className="navbar-releases-container">
-      <p>link Proximo Lançamento</p>
-      <p>link ultimo lançamento</p>
-
-      <p> link Próximos lançamentos</p>
-      <p> linkLançamentos passados</p>
+      <Link to="/release/next">Proximo Lançamento</Link>
+      <Link to="/release/latest">Último lançamento</Link>
+      <Link to="/releases/next">Próximos lançamentos</Link>
+      <Link to="/releases/next">Lançamentos passados</Link>
     </div>
   )
 }
