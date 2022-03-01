f339cc60733d5e825a4d96e15dee53c8778ba594
Carol_Inglethe <carolinainglethe@gmail.com>
2022-03-01 11:42:09 -0300

    CSS pagina Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import NavBar from '../components/NavBar';
 
 function Home() {
@@ -6,9 +7,11 @@ function Home() {
   return (
     <div className="home-container">
      <NavBar />
-     <div>
-         Home
-     </div>
+     <section className="home-section">
+        <h1>Seja Bem vindo(a) ao Space-X</h1>
+        <p>Veja os lançamentos e suas informações</p>
+        <button type="button"> <Link to="/releases"> Veja aqui </Link> </button>
+     </section>
     </div>
   );
 }
