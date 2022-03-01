72ccba02a3bd9b0c7dcd7ca74f7159346c6f4e59
Carol_Inglethe <carolinainglethe@gmail.com>
2022-03-01 20:19:10 -0300

    TableReleases - trata tabela para cada tipo de informaçao recebido

--- src/components/TableReleases.jsx
+++ src/components/TableReleases.jsx
@@ -2,15 +2,35 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const TableReleases = ({ infoRealeses }) => {
-  const columns = [ "Nome", "Numero do voo" , "Data/Hora de lançamento", "Foguete" ]
+  const columns = [ "Nome", "Numero do voo" , "Data/Hora de lançamento", " Id Foguete" ]
   
   console.log(infoRealeses)
 
   if(!infoRealeses) return <p>Carrengando...</p>
 
-  if( typeof(infoRealeses) === 'object') {
-    <div className="table-releases-container">
-      string
+  if( infoRealeses.name) {
+    return <div className="table-releases-container">
+      <table>
+     <thead>
+        <tr>
+          {
+            columns.map((colum, index) => (
+              <th key={index} >{colum}</th>
+            ))
+          }
+        </tr>
+      </thead>
+      <tbody>
+        {
+            <tr>
+              <td>{infoRealeses.name}</td>
+              <td>{infoRealeses.flight_number}</td>
+              <td>{Date(infoRealeses.date_utc)}</td>
+              <td>{infoRealeses.rocket}</td>
+            </tr>
+        }
+      </tbody>
+     </table>
     </div>
   }
 
@@ -29,17 +49,16 @@ const TableReleases = ({ infoRealeses }) => {
         </tr>
       </thead>
       <tbody>
-        {/* {
+        {
           infoRealeses.map((obj) => (
             <tr>
               <td>{obj.name}</td>
+              <td>{obj.flight_number}</td>
+              <td>{Date(obj.date_utc)}</td>
+              <td>{obj.rocket}</td>
             </tr>
           ))
-        } */}
-        <tr>
-          <td>x</td>
-          <td>x</td>
-        </tr>
+        }
       </tbody>
      </table>
     </div>
