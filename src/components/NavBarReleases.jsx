745a7a4351650eff5160e730128a2c33a97fb8d8
Carol_Inglethe <carolinainglethe@gmail.com>
2022-03-01 17:56:50 -0300

    modifica classes , organiza e coloca link todos no navbarReleases

--- src/components/NavBarReleases.jsx
+++ src/components/NavBarReleases.jsx
@@ -4,9 +4,10 @@ import { Link } from 'react-router-dom';
 const NavBarReleases = () => {
 
   return (
-    <div >
+    <div className="navbar-releases-container">
       <h1>Lançamentos</h1>
-      <div className="navbar-releases-container">
+      <div className="navbar-releases-links">
+        <Link to="/releases"> Todos </Link>
         <Link to="/release/next">Próximo Lançamento</Link>
         <Link to="/release/latest">Último lançamento</Link>
         <Link to="/releases/upcoming">Próximos lançamentos</Link>
